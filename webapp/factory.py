c412ccf2656e281f2901b35909428cdf51ec0a36
Andres Morey <andres@octopart.com>
2014-06-08 19:50:53 -0700

    first commit

---
+++ webapp/factory.py
@@ -0,0 +1,23 @@
+import pkgutil
+import importlib
+
+from flask import Flask
+
+
+def create_app(package_name, package_path, settings_override=None):
+    """Create app
+    """
+    app = Flask(package_name, instance_relative_config=True)
+
+    app.config.from_object('config')
+    app.config.from_object(settings_override)
+
+    # register blueprints
+    for _, name, _ in pkgutil.iter_modules(package_path):
+        m = importlib.import_module('%s.%s' % (package_name, name))
+        for item in dir(m):
+            item = getattr(m, item)
+            if isinstance(item, Blueprint):
+                app.register_blueprint(item)
+
+    return app
---
+++ wsgi.py
@@ -0,0 +1,16 @@
+from werkzeug.serving import run_simple
+from werkzeug.wsgi import DispatcherMiddleware
+
+from webapp import create_app
+from webapp import bp1, bp2
+
+create_app('webapp.bp1')
+
+application = DispatcherMiddleware(
+    bp1.create_app(),
+    {'/bp2': bp2.create_app()}
+    )
+
+if __name__ == "__main__":
+    run_simple('0.0.0.0', 5000, application, use_reloader=True,
+               use_debugger=True)
