c8cbe35a8093ac8be127ed623b12434ae98becd2
Andres Morey <andres@octopart.com>
2014-06-17 21:28:17 -0700

    improvement to unit tests

--- tests/test_bp1.py
+++ tests/test_bp1.py
@@ -9,25 +9,21 @@ class TestCase(unittest.TestCase):
     # Test-level setup/teardown
     # ==============================
     def setUp(self):
-        app = bp1.create_app(extra_config={'TESTING': True})
-        self.app = app.test_client()
+        self.app = bp1.create_app(extra_config={'TESTING': True})
+        self.client = self.app.test_client()
 
     # ==============================
     # Test methods
     # ==============================
     def test_path1(self):
-        resp = self.app.get('/')
+        resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertTrue('<title>Webapp</title>' in resp.data)
 
     def test_send_email(self):
         with mail.record_messages() as outbox:
-            resp = self.app.get('/send-email')
+            resp = self.client.get('/send-email')
             self.assertEqual(resp.status_code, 200)
             self.assertEqual(resp.data, 'ok')
             self.assertEqual(len(outbox), 1)
             self.assertEqual(outbox[0].subject, 'Subject')
-
-
-if __name__ == '__main__':
-    unittest.main()
--- tests/test_bp2.py
+++ tests/test_bp2.py
@@ -8,17 +8,13 @@ class TestCase(unittest.TestCase):
     # Test-level setup/teardown
     # ==============================
     def setUp(self):
-        app = bp2.create_app(extra_config={'TESTING': True})
-        self.app = app.test_client()
+        self.app = bp2.create_app(extra_config={'TESTING': True})
+        self.client = self.app.test_client()
 
     # ==============================
     # Test methods
     # ==============================
     def test_path1(self):
-        resp = self.app.get('/')
+        resp = self.client.get('/')
         self.assertEqual(resp.status_code, 200)
         self.assertFalse('<title>Webapp</title>' in resp.data)
-
-
-if __name__ == '__main__':
-    unittest.main()
