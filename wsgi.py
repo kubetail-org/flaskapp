68316a6caabfea1f0a1b4d6204e930e47a6120ad
Andres Morey <andres@octopart.com>
2014-08-19 18:57:43 -0400

    added threading to wsgi app

--- wsgi.py
+++ wsgi.py
@@ -9,4 +9,4 @@ application = create_app()
 
 if __name__ == "__main__":
     run_simple('0.0.0.0', 5000, application, use_reloader=True,
-               use_debugger=True)
+               use_debugger=True, threaded=True)
