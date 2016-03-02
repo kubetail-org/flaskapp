d57bfe08d66b735331c4ea2f233eecca59273a78
Andres Morey <andresmarcel@gmail.com>
2016-03-02 08:06:54 -0500

    improved wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,15 @@
-from werkzeug.serving import run_simple
-from werkzeug.wsgi import DispatcherMiddleware
-
 from flaskapp import create_app
 
 
-application = create_app()
+app = create_app()
 
 
 if __name__ == "__main__":
-    run_simple('0.0.0.0', 5000, application, use_reloader=True,
-               use_debugger=True, threaded=True)
+    from werkzeug.serving import run_simple
+
+    run_simple('0.0.0.0',
+               5000,
+               app,
+               threaded=True,
+               use_reloader=True,
+               use_debugger=True)
